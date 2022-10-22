b80feaae0c26fb833e209e507f5d21a1fe7b80e6
Anne Vaz <annevaz@yahoo.com.br>
2022-10-22 16:57:51 -0300

    Initial project

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# aws-atividade-1
-Protocolo HTTP e SOAP
+# APIs e Web Services
+
+## Atividade-1
+
+Protocolo HTTP e SOAP.
+
+## Web Service
+
+[Country Information](http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso)
+
+[Country Information WSDL](http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?wsdl)
+
+## Tool used in VS Code
+
+[WCF dotnet-svcutil tool for .NET Core](https://learn.microsoft.com/en-us/dotnet/core/additional-tools/dotnet-svcutil-guide?tabs=dotnetsvcutil2x) retries metadata from a web service from a WSDL file, and generates a WCF class containing client proxy methods that access the web service operations.
---
+++ aws-atividade-1/Program.cs
@@ -0,0 +1,72 @@
+﻿using static ServiceReference.CountryInfoServiceSoapTypeClient;
+
+Console.BackgroundColor = ConsoleColor.Blue;
+
+Console.Write("DataFlex Web Service for Country information");
+
+Console.BackgroundColor = ConsoleColor.Black;
+
+Console.WriteLine("\n");
+
+var client = new ServiceReference.CountryInfoServiceSoapTypeClient(EndpointConfiguration.CountryInfoServiceSoap);
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine("List of Continents");
+
+Console.ForegroundColor = ConsoleColor.White;
+
+var continents = client.ListOfContinentsByNameAsync().Result.Body.ListOfContinentsByNameResult;
+
+foreach (var continent in continents)
+{
+    Console.WriteLine($"\t{continent.sCode} - {continent.sName}");
+}
+
+Console.WriteLine("\n");
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine("Searching Brazil by ISO Code");
+
+Console.ForegroundColor = ConsoleColor.White;
+
+var countries = client.ListOfCountryNamesByCodeAsync().Result.Body.ListOfCountryNamesByCodeResult;
+
+string brazilISOCode = "BR";
+
+var brazil = countries
+    .Where(country => country.sISOCode.Equals(brazilISOCode))
+    .FirstOrDefault();
+
+if (brazil != null)
+{
+    Console.WriteLine($"\t{brazil.sName}");
+}
+
+Console.WriteLine("\n");
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine("Informations of Brazil");
+
+Console.ForegroundColor = ConsoleColor.White;
+
+string brazilCurrencyISOCode = "BRL";
+
+var brazilInfo = client.FullCountryInfoAsync(brazilCurrencyISOCode).Result.Body.FullCountryInfoResult;
+
+Console.WriteLine($"\tContinent code: {brazilInfo.sContinentCode}");
+Console.WriteLine($"\tISO Code: {brazilInfo.sISOCode}");
+Console.WriteLine($"\tCapital city: {brazilInfo.sCapitalCity}");
+Console.WriteLine($"\tInternational phone code: {brazilInfo.sPhoneCode}");
+Console.WriteLine($"\tFlag: {brazilInfo.sCountryFlag}");
+
+Console.WriteLine("\tLanguages");
+
+var brazilLanguages = brazilInfo.Languages;
+
+foreach (var brazilLanguage in brazilLanguages)
+{
+    Console.WriteLine($"\t\t{brazilLanguage.sISOCode} - {brazilLanguage.sName}");
+}
